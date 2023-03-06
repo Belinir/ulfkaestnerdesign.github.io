cf260664d43798aa7326653c9e70e2b35c002d22
Ulf K <kaestnerulf@gmail.com>
2023-03-06 19:14:38 +0100

    Only half my javascript is working 👌

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+/*eslint-env es6*/
+const menu = document.getElementById("menu");
+
+Array.from(document.getElementsByClassName("menu-item"))
+	.forEach((item, index) => {
+		item.onmouseover = () => {
+			menu.style.setProperty("-active-index",index)
+		}
+});// JavaScript Document
