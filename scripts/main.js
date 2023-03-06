effc19ac0a1328e844eae81aa86564fad8c8175e
Ulf K <kaestnerulf@gmail.com>
2023-03-06 19:59:16 +0100

    Bruh Javascript, work

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ const menu = document.getElementById("menu");
 Array.from(document.getElementsByClassName("menu-item"))
 	.forEach((item, index) => {
 		item.onmouseover = () => {
-			menu.style.setProperty("-active-index",index)
+			menu.dataset.activeIndex = index;
 		}
+console.log("BIMBAM");
+alert("BAMBIM");
 });// JavaScript Document
